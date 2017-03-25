a85b73c568ad4a3c570cb700d4e3c0dbca6b673c
Francis Bergin <bergin.francis@gmail.com>
2017-03-25 16:02:17 -0400

    Change rad_can implementation

--- rad_can.py
+++ rad_can.py
@@ -1,60 +1,75 @@
 import copy
 import itertools
 import json
+import os
 import random
 
 import requests
 
 
-_theme_articles = []
+_articles = []
+ARTICLES_FILE = 'articles.json'
+THEMES_BLACKLIST = [14, 16, 21, 23]
 
 
-def is_valid_article(article):
+def _is_valid_article(article):
     if article['contentType']['name'] == 'Nouvelle':
         return True
     return False
 
 
-def get_articles(max_number):
-    global _theme_articles
-    articles = []
-    for i in itertools.cycle(range(len(_theme_articles))):
+def load_articles(number=100, from_file=False):
+    global _articles
+    if from_file and os.path.isfile(ARTICLES_FILE):
+        with open(ARTICLES_FILE) as f:
+            _articles = json.load(f)
+        return len(_articles)
+    themes = _get_theme_lineups()
+    loaded_number = 0
+    for i in itertools.cycle(range(len(themes))):
         try:
-            articles.append(next(_theme_articles[i]['generator']))
-            print('got article')
+            _articles.append(next(themes[i]['generator']))
+            loaded_number += 1
+            print(loaded_number)
         except StopIteration:
-            _theme_articles[i]['generator'] = \
-                get_theme_articles(_theme_articles[i]['href'])
-        if len(articles) == max_number:
+            themes[i]['generator'] = _get_theme_articles(themes[i]['href'])
+        if len(_articles) == number:
             break
-        if not list(filter(lambda x: x is not None, _theme_articles)):
+        if not list(filter(lambda x: x is not None, themes)):
             break
-    return articles
+    with open(ARTICLES_FILE, 'w') as f:
+        json.dump(_articles, f)
+    return loaded_number
 
 
-def get_theme_articles(href):
-    r = requests.get(href)
+def _get_theme_articles(href):
     print(href)
+    r = requests.get(href)
     for article in r.json()['pagedList']['items']:
-        if is_valid_article(article):
+        if _is_valid_article(article):
             yield article
-        else:
-            print('not valid')
     if r.json()['pagedList'].get('nextPageLink'):
-        yield from get_theme_articles(r.json()['pagedList']['nextPageLink']['href'])
+        yield from _get_theme_articles(r.json()['pagedList']['nextPageLink']['href'])
 
 
-def load_theme_lineups():
-    global _theme_articles
-    _theme_articles = []
-
+def _get_theme_lineups():
+    theme_lineups = []
     with open('themes.json') as f:
         themes = json.load(f)['themes']
-
     random.shuffle(themes)
-
+    themes = [t for t in themes if t['id'] not in THEMES_BLACKLIST]
     for theme in themes:
-        _theme_articles.append({
+        theme_lineups.append({
             'href': theme['lineupLink']['href'],
-            'generator': get_theme_articles(theme['lineupLink']['href']),
+            'generator': _get_theme_articles(theme['lineupLink']['href']),
         })
+    return theme_lineups
+
+
+def get_articles(number):
+    global _articles
+    if not _articles:
+        load_articles(number=number, from_file=False)
+    articles = _articles[:number]
+    _articles = _articles[number:]
+    return articles
