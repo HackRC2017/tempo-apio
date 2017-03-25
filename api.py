c9454c8a206bd42a6ec278ad7e2996d62eb1118a
Francis Bergin <bergin.francis@gmail.com>
2017-03-25 14:52:13 -0400

    Integrate rad_can into main api

--- api.py
+++ api.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
+import rad_can
+rad_can.load_theme_lineups()
+
 
 __version__ = '0.1.0'
 app = Flask(__name__)
@@ -7,3 +11,10 @@ app = Flask(__name__)
 @app.route('/version')
 def version():
     return __version__
+
+
+@app.route('/articles')
+def articles():
+    time = request.args.get('time', default=10, type=int)
+    number = request.args.get('number', default=10, type=int)
+    return jsonify(rad_can.get_articles(number))
