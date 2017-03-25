bf7286df386146ec98d7ffd019f6662757edf1fc
Francis Bergin <bergin.francis@gmail.com>
2017-03-25 18:40:41 -0400

    Remove rad_can code from tempo-apio

--- tempo_apio.py
+++ tempo_apio.py
@@ -1,18 +1,8 @@
-from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, jsonify, request
-
-import rad_can
 
 
 __version__ = '0.1.0'
 app = Flask(__name__)
-
-
-@app.before_first_request
-def _run_on_start():
-    scheduler = BackgroundScheduler()
-    scheduler.start()
-    scheduler.add_job(rad_can.get_new_articles, 'interval', minutes=5)
 
 
 @app.route('/version')
@@ -24,4 +14,4 @@ def version():
 def articles():
     time = request.args.get('time', default=10, type=int)
     number = request.args.get('number', default=10, type=int)
-    return jsonify(rad_can.get_articles(number))
+    return jsonify([])
